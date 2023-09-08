b613ee3fcf1c253a3dace4cc40b1f8ce0207b699
aleighwood <aleighwood@gmail.com>
2023-09-08 11:00:16 +0100

    remove PHP, use JS and HTML

---
+++ Scripts/sidebar.js
@@ -0,0 +1,20 @@
+sideBarHTML=`
+
+<a href='aboutPage.html' class='nostyle sidebar-title '>About</a>
+
+<a href='projectPage.html' class='nostyle sidebar-title '>Projects</a>
+
+<a href='contactPage.html' class='nostyle sidebar-title '>Contact</a>
+
+<a href='shapesPage.html' class='nostyle sidebar-title '>Shapes</a>
+
+<a href='DifDafPage.html' class='difdaf nostyle sidebar-title '>Dif Daf</a>
+<!--
+<a href='cardsPage.html' class='nostyle sidebar-title '>Cards</a>
+
+<a href='buttonsPage.html' class='nostyle sidebar-title '>Buttons</a>
+-->
+`;
+
+sideBarElement = document.querySelector('.js-sidebar');
+sideBarElement.innerHTML = sideBarHTML;
--- modularContent/sidebar.php
+++ modularContent/sidebar.php
@@ -1,6 +1,6 @@
 <nav class="sidebar">
 
-  <a href="index.php" class="nostyle sidebar-title ">About</a>
+  <a href="aboutPage.php" class="nostyle sidebar-title ">About</a>
 
   <a href="projectPage.php" class="nostyle sidebar-title ">Projects</a>
 
