5722dccf3c022c68b47b950b1c5824b33714e907
aleighwood <aleighwood@gmail.com>
2025-09-21 19:51:02 +0100

    update about and projects

--- Data/projects.js
+++ Data/projects.js
@@ -1,7 +1,36 @@
 export const projects = [
+
+		{
+		name: 'Framework C02 monitor expansion card',
+		id: '001',
+		emoji: '&#128067',
+		year: '2025',
+		category: 'Electronics',
+		url: 'https://github.com/aleighwood/FrameworkExpansionCard'
+	},
+
+	{
+		name: 'FM radio FPGA implementation',
+		id: '002',
+		emoji: '&#128251',
+		year: '2025',
+		category: 'Electronics',
+		url: 'https://github.com/aleighwood/FM-radio-FPGA-implementation-Northwestern-COMP_ENG_387'
+	},
+
+		{
+		name: 'Diagnosing Alzheimer\'s using ML',
+		id: '003',
+		emoji: '&#129301',
+		year: '2024',
+		category: 'Machine Learning',
+		url: 'https://github.com/aleighwood/ELEC_ENG_375_FINAL'
+	},
+
+
 	{
 		name: 'Sleep timer',
-		id: '001',
+		id: '004',
 		emoji: '&#9200',
 		year: '2021',
 		category: 'Electronics',
@@ -9,7 +38,7 @@ export const projects = [
 	},
 	{
 		name: 'Force sensor',
-		id: '002',
+		id: '005',
 		emoji: '&#11015',
 		year: '2023',
 		category: 'Electronics',
@@ -17,26 +46,31 @@ export const projects = [
 	},
 	{
 	name: 'This website',
-	id: '004',
+	id: '006',
 	emoji: '&#127760',
 	year: '2023',
 	category: 'Websites',
 	url: 'https://github.com/aleighwood/personalWebsite'},
 
 	{
-	name: 'System Verilog CPU design',
-	id: '005',
+	name: 'SystemVerilog CPU design',
+	id: '007',
 	emoji: '&#x1f4bb',
 	year: '2024',
 	category: 'Electronics',
 	url: 'https://github.com/aleighwood/system_verilog_design'},
 
-	{
+
+
+];
+
+
+	/*
 		name: 'Sleep movement sensor',
 		id: '006',
 		emoji: '&#x1f634;',
 		year: '2024',
 		category: 'Electronics',
 		url: 'https://github.com/aleighwood/movement_sensor_array'}
-
-];
+	*/
+		
